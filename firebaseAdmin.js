12a57fd0905ecc080e664c3eb2d63db014d4bdf4
ErvinClay13 <ervinclay88@gmail.com>
2026-03-13 00:19:47 -0500

    Fix firebase admin import for render

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,9 +1,20 @@
 import admin from "firebase-admin";
-import serviceAccount from "./firebase-admin.json" assert { type: "json" };
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Needed for ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Read service account file
+const serviceAccount = JSON.parse(
+  fs.readFileSync(path.join(__dirname, "firebase-admin.json"), "utf8")
+);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
 export const adminDb = admin.firestore();
-export default admin;
+export const adminAuth = admin.auth();
